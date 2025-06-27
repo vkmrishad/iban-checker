b86b92f6cbee58520172f10fc1d527f3b0c1ab36
Mohammed Rishad <mohammedrishad.vk@gmail.com>
2025-06-27 19:23:51 +0530

    Update vase url for gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/iban-checker/',
+  // base: '/iban-checker/',
+  base: '/', // <- IMPORTANT for custom domains
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
